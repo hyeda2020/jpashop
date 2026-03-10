32d866912ea02da4d34dbda7b25da9bc5d395518
hyeda2020 <hyeda2020@gmail.com>
2026-03-10 22:51:38 +0900

    엔티티 클래스 개발 1

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Address {
+
+    String city;
+    String street;
+    String zipcode;
+}
