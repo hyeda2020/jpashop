d2ec29596168d3f0deacc62a03e237f35fc7efc3
hyeda2020 <hyeda2020@gmail.com>
2026-03-12 22:34:26 +0900

    Category 엔티티 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,16 @@ public class Address {
     String city;
     String street;
     String zipcode;
+
+    // JPA 스펙상 엔티티나 임베디드 타입은
+    // public/protected 로 선언된 기본 생성자가 필요하여 선언만 해둠.(실제 사용하지 않기에 protected 로 선언)
+    protected  Address() {
+    }
+
+    // @Setter 대신에 생성자에 값을 초기화해서 변경 불가능 클래스로 만듦.
+    public Address(Category parent, String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
