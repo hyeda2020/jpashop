f58f83ebb74d85f8bb6913a8638d6e7964f03ab5
hyeda2020 <hyeda2020@gmail.com>
2026-03-15 00:46:52 +0900

    상품 서비스 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { // 신규 아이템이면
+            em.persist(item); // 신규 등록
+        } else {
+            em.merge(item); // 이미 등록되어 있는 아이템이면 업데이트
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
