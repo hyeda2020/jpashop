c0d8f03ae7140fde329e9c6e560f6e7a44df8687
hyeda2020 <hyeda2020@gmail.com>
2026-03-17 20:19:13 +0900

    웹 계층 개발 1

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -10,6 +10,12 @@ import java.util.List;
 @Entity
 @Getter @Setter
 public class Member {
+    /**
+     * 엔티티는 순수하게 DB와 관련되게끔만 설계해야 됨.
+     * 화면과 관련된건 별도 Form 클래스를 생성하여 활용
+     * 또한, 엔티티는 절대로 화면에 노출되면 안됨
+     * (보안상 이슈 및 엔티티 스펙이 바뀔때마다 화면 필드 및 API 구조도 같이 바뀌어야됨.)
+     */
 
     @Id @GeneratedValue
     @Column(name = "member_id")
--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -13,4 +13,14 @@ public class Book extends Item {
 
     private String author;
     private String isbn;
+
+    public static Book createBook(Long id, String name, int price, int stockQuantity, String author, String isbn) {
+        Book book = new Book();
+        book.setId(id);
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        book.setAuthor(author);
+        return book;
+    }
 }
